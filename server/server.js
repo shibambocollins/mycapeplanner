2a39edb598898626a44c8db962813dfd3be03bcb
shibambocollins <ntsobokwanec@gmail.com>
2026-07-16 23:02:37 +0200

    refactor: streamline CORS configuration by removing duplicate code

--- server/server.js
+++ server/server.js
@@ -16,27 +16,6 @@ const allowedOrigins = [
   'http://127.0.0.1:5174',
 ].filter(Boolean);
 
-const app = express();
-const allowedOrigins = [
-  process.env.CLIENT_ORIGIN,
-  'http://localhost:5173',
-  'http://localhost:5174',
-  'http://127.0.0.1:5173',
-  'http://127.0.0.1:5174',
-].filter(Boolean);
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin) || /\.vercel\.app$/.test(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-  })
-);
-
 app.use(
   cors({
     origin: (origin, callback) => {
